9a7f9032d05c528705150d916b594e23139dfc0b
PaulinaKapka <kapkapaulina@icloud.com>
2018-05-26 13:36:56 +0200

    changed country key

--- app/redux/apiData/actions.js
+++ app/redux/apiData/actions.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import API_KEY from '../../../api_config.json'
 
 export function getTemperature() {
-  const url = `https://api.weatherbit.io/v2.0/current?city=Poznan&country=Poland&key=${API_KEY}`
+  const url = `https://api.weatherbit.io/v2.0/current?city=Poznan&country=PLd&key=${API_KEY}`
 
   axios.get(url)
     .then((response) => {
