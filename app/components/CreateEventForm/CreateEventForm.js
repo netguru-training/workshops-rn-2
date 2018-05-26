fc3aafbc04b5075fc138a2c95d359eac4b4af702
Dawid Urbas <dawid.urbas@sap.com>
2018-05-26 14:09:29 +0200

    Fix input state

--- app/components/CreateEventForm/CreateEventForm.js
+++ app/components/CreateEventForm/CreateEventForm.js
@@ -1,26 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 import { Input } from '../'
 import styles from './CreateEventForm.styles'
 
 const { container } = styles
+class CreateEventForm extends Component {
+  state = {
+    input: ''
+  }
 
-const CreateEventForm = () => {
-  let inputState = ''
-
-  return (
-    <View style={container}>
-      <Text>Sample Text</Text>
-      <Input
-        label='Label'
-        placeholder='Text'
-        value={inputState}
-        onChangeText={(value) => {
-          inputState = value
-        }}
-      />
-    </View>
-  )
+  render() {
+    return (
+      <View style={container}>
+        <Text>Sample Text</Text>
+        <Input
+          label="Label"
+          placeholder="Text"
+          value={this.state.input}
+          onChangeText={(value) => this.setState({ input: value })}
+        />
+      </View>
+    )
+  }
 }
 
 export default CreateEventForm
